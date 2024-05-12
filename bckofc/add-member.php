69af53ace4bf97062f3771e893cc91ae50252972
koiescafe2024 <168373038+koiescafe2024@users.noreply.github.com>
2024-05-13 00:42:18 +0530

    Update add-member.php

--- bckofc/add-member.php
+++ bckofc/add-member.php
@@ -398,6 +398,14 @@ include 'layout/header.php';
   <!-- /.content -->
 </div>
 
+  <script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+
 <?php
 include 'layout/footer.php';
 ?>
