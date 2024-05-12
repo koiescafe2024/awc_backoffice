1b059ca1429869d10f40b584b2b1377f9d799e57
koiescafe2024 <168373038+koiescafe2024@users.noreply.github.com>
2024-05-13 03:37:47 +0530

    Create PLReport.php

---
+++ bckofc/PLReport.php
@@ -0,0 +1,121 @@
+<?php
+  include 'layout/header.php'; 
+  ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+  <div class="content-header">
+      <div class="container-fluid">
+        
+        <div class="row mb-2">
+          <div class="col-sm-6">
+
+          <div class="col-sm-3"></div>
+            <h1>Win / Loss Report</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Reports</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Win / Loss Stats</h3>
+
+                <div class="card-tools">
+                Filters
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <table class="table" >
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>User ID</th>
+                      <th>User Name</th>
+                      <th>Platform</th>
+                      <th>Type</th>
+                      <th>Location</th>
+                      <th>Bet Count</th>
+                      <th>Valid Turnover</th>
+                      <th>Bet Amount</th>
+                      <th>Total Bet</th>
+                      <th><span style="color:blue;font-weight:bold">Player</span> Win/Loss</th>
+                      <th>Adjustment</th>
+                      <th>Total P/L</th>
+                      <th>Margin(%)</th>
+                      <th><span style="color:blue;font-weight:bold">Agent</span>PT Win/Loss</th>
+                      <th>Adjustment</th>
+                      <th>Total P/L</th>
+                     
+                      <th><span style="color:blue;font-weight:bold">Franchise Agent</span> Win/Loss</th>
+                      <th>Adjustment</th>
+                      <th>Total P/L</th>
+                     
+                      <th><span style="color:blue;font-weight:bold">Company</span>Total P/L</th>
+                     
+                     
+                      <th>Status</th>
+                      <th colspan="3" style=" width: 90px;">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td ></td>
+                      <td ></td>
+                      <td></td>
+                      <td ></td>
+                      <td></td>
+                      <td ></td>
+                      <td></td>
+                      <td ></td>
+                      <td></td>
+                      <td></td>
+                      <td ></td>
+                      <td ></td>
+                      <td></td>
+                      <td ></td>
+                      <td></td>
+                      <td ></td>
+                      <td></td>
+                      <td ></td>
+                      <td></td>
+                      <td></td>
+                       <td ></td>
+                      <td ></td>
+                      <td></td>
+                      
+                     
+                     
+                    </tr>
+
+
+                    
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+           
+               
+                 
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>         
+
+  <?php
+  include 'layout/footer.php'; 
+  ?>
+ 
