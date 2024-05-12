9aebd9aaf5204a89f5c1d070352c6b1151d9a56f
koiescafe2024 <168373038+koiescafe2024@users.noreply.github.com>
2024-05-13 01:18:38 +0530

    Update add-member.php

--- bckofc/add-member.php
+++ bckofc/add-member.php
@@ -48,7 +48,7 @@ include 'layout/header.php';
               <div class="card-body">
                 <div class="tab-content" id="custom-tabs-five-tabContent">
                  
-                  <div class="tab-pane fade" id="custom-tabs-five-normal" role="tabpanel" aria-labelledby="custom-tabs-five-normal-tab">
+                  <div class="tab-pane" id="custom-tabs-five-normal" role="tabpanel" aria-labelledby="custom-tabs-five-normal-tab">
                     Mauris tincidunt mi at erat gravida, eget tristique urna bibendum. Mauris pharetra purus ut ligula tempor, et vulputate metus facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas sollicitudin, nisi a luctus interdum, nisl ligula placerat mi, quis posuere purus ligula eu lectus. Donec nunc tellus, elementum sit amet ultricies at, posuere nec nunc. Nunc euismod pellentesque diam.
                   </div>
 
