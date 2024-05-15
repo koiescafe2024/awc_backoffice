5c5c4ff892ce09fde515e51a7af84a3f1794cfa5
koiescafe2024 <168373038+koiescafe2024@users.noreply.github.com>
2024-05-15 08:48:44 +0530

    Update index.php

--- bckofc/index.php
+++ bckofc/index.php
@@ -23,6 +23,23 @@
   <?php 
   include "vendor/autoload.php";
 
+  if (isset($argv[1])) {
+    $secretKey = $argv[1];
+    echo "Secret passed in as argument!\n";
+    echo "Your secret is ".$secretKey."\n";
+} else {
+    $secretFactory = new \Dolondro\GoogleAuthenticator\SecretFactory();
+    $secret = $secretFactory->create("MyAwesomeWebCo", "Dolondro");
+    $secretKey = $secret->getSecretKey();
+
+    $qrImageGenerator = new \Dolondro\GoogleAuthenticator\QrImageGenerator\EndroidQrImageGenerator();
+    // $qrImageGenerator = new \Dolondro\GoogleAuthenticator\QrImageGenerator\GoogleQrImageGenerator();
+
+    echo "Your secret is: ".$secretKey."\n";
+    file_put_contents(__DIR__."/example.html", "<img src='".$qrImageGenerator->generateUri($secret)."'>'");
+    echo "Visit this URL: 'file://".__DIR__."/example.html' to view an image of your secret, and add it to your google authenticator app\n";
+}
+
   ?>
   <div class="card">
     <div class="card-body login-card-body">
